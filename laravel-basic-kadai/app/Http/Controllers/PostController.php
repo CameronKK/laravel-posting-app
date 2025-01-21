5a9793bcb2826dc6819c0d1fe52640a902c759af
Cameron <ktomotomo58sky@gmail.com>
2025-01-21 12:27:51 +0900

    second change

--- laravel-basic-kadai/app/Http/Controllers/PostController.php
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -17,4 +17,24 @@ class PostController extends Controller
         $post = Post::find($id);
         return view('posts.show', compact('post'));
     }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts', 'post'));
+    }
 }
